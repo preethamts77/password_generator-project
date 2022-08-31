549990e66c2ca0c3c790fcc2c76212aeed052476
BeastCode <preethamts5@gmail.com>
2022-08-31 12:11:50 +0530

    my secong commit!

--- generator/views.py
+++ generator/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>THE BEAST</h1>')
+    return HttpResponse('THE BEAST')
 
 def eggs(request):
     return HttpResponse('<h1>Eggs are so tasty if cooked good</h1>')
